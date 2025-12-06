4b8675d4336dc0522946d87db2d76936a7ca74c7
Oswaldo Dantas <oswaldodantas@gmail.com>
2025-12-06 02:13:12 +0100

    fix(logging): standardize to ISO 8601 and .log extension

--- src/main/scala/first/Logging.scala
+++ src/main/scala/first/Logging.scala
@@ -6,8 +6,18 @@ import scribe.format.*
 
 object Logging:
   def configure(): Unit =
+    import java.time.format.DateTimeFormatter
+    import java.time.Instant
+    import java.time.ZoneId
+    import scribe.output.TextOutput
+
+    val dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault())
+    val isoDate = FormatBlock { logRecord =>
+      new TextOutput(dtf.format(Instant.ofEpochMilli(logRecord.timeStamp)))
+    }
+
     val cliFormatter  = formatter"$messages$newLine"
-    val fileFormatter = formatter"$date $level $position - $messages$newLine"
+    val fileFormatter = formatter"$isoDate $level $position - $messages"
 
     Logger.root
       .clearHandlers()
@@ -19,7 +29,8 @@ object Logging:
       .withHandler(
         formatter = fileFormatter,
         minimumLevel = Some(Level.Debug),
-        writer = FileWriter(System.getProperty("user.dir") / ".first" / "logs" / daily()),
+        writer =
+          FileWriter(System.getProperty("user.dir") / ".first" / "logs" / (year % "-" % month % "-" % day % ".log")),
         outputFormat = scribe.output.format.ASCIIOutputFormat,
       )
       .replace()
